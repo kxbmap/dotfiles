2e53b2482299f658ade35134db774c9a5431cec4
kxbmap <kxbmap@gmail.com>
2014-05-30 09:10:38 +0900

    Add PSReadline settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,10 @@
 Import-Module Pscx -arg (Join-Path (Split-Path $Profile) Pscx.UserPreferences.ps1)
 
 Import-Module PSReadline
+
+Set-PSReadlineOption -BellStyle None
+
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineKeyHandler -Key Ctrl+Spacebar -Function TabCompleteNext
+Set-PSReadlineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key Ctrl+n -Function HistorySearchForward
