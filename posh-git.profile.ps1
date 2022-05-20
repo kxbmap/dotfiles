443f3b01e923257dc789ff2d6575e0d348c90d69
kxbmap <kxbmap@gmail.com>
2022-05-20 22:35:57 +0900

    remove unsupported option

--- posh-git.profile.ps1
+++ posh-git.profile.ps1
@@ -3,5 +3,4 @@ Import-Module posh-git
 $Env:TMPDIR = $Env:TMP
 # Start-SshAgent -Quiet
 
-$GitPromptSettings.EnableWindowTitle = $true
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
